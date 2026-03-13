cbe2ff122572adddf3c789e5d13d4ab2a02946c9
fsoares <franciscolsoares8@gmail.com>
2026-03-13 15:39:25 +0100

    Call feature extraction from worker executor

--- worker/executor.py
+++ worker/executor.py
@@ -1,9 +1,11 @@
 import logging
 
+from tasks import feature_extraction
+
 logger = logging.getLogger(__name__)
 
 
 def execute_task(task: dict) -> None:
     task_id = task["task_id"]
     logger.info("Executing task %s", task_id)
-    print(f"Executing task {task_id}")
+    feature_extraction.run(task)
