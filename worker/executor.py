ef7dd58e992f99e377227cb556ad7269f3f6ea3f
fsoares <franciscolsoares8@gmail.com>
2026-03-13 16:42:25 +0100

    Adapt worker executor for both task types

--- worker/executor.py
+++ worker/executor.py
@@ -1,11 +1,17 @@
 import logging
 
-from tasks import feature_extraction
+from tasks import feature_extraction, reduce_stage
 
 logger = logging.getLogger(__name__)
+
+TASK_HANDLERS = {
+    "feature_extraction": feature_extraction.run,
+    "reduce": reduce_stage.run,
+}
 
 
 def execute_task(task: dict) -> None:
     task_id = task["task_id"]
-    logger.info("Executing task %s", task_id)
-    feature_extraction.run(task)
+    task_type = task["type"]
+    logger.info("Executing task %s (type: %s)", task_id, task_type)
+    TASK_HANDLERS[task_type](task)
